1820e7783d5ac7af9a71f3ba5b091f7f9d8ee34c
Robert Pelger <pelgero@gmail.com>
2019-05-31 10:33:53 +0200

    Added additional Javadoc

--- src/main/java/dev/pelgero/jonads/Result.java
+++ src/main/java/dev/pelgero/jonads/Result.java
@@ -16,8 +16,8 @@ import java.util.function.*;
 public interface Result<E, T> {
 
     /**
-     * Basic factory method for wrapping an operation that might throw an (runtime) exception in a Result. If provided
-     * operation returns normally, you'll get an Ok(T), but if operation throws an exception you'll get a Err(E)
+     * Invokes supplier, if operation succeeds, returns the result in an Ok. If operation throws a (runtime)
+     * exception, wraps the exception in an Err
      *
      * @param operation that might fail with an exception
      * @param <T> the expected (normal) result type of operation
@@ -32,14 +32,14 @@ public interface Result<E, T> {
     }
 
     /**
-     * Whether this Result is an Ok(T) or an Err(E)
-     * @return <code>true</code> when Ok(T), <code>false</code> when Err(E)
+     * Returns <code>true</code> when this Result is an Ok, otherwise <code>false</code>
+     * @return  <code>true</code> when Ok, <code>false</code> otherwise
      */
     boolean isOk();
 
     /**
-     * Whether this Result is an Ok(T) or an Err(E)
-     * @return <code>true</code> when Err(E), <code>false</code> when Ok(E)
+     * Returns <code>true</code> when this Result is an Err, otherwise <code>false</code>
+     * @return  <code>true</code> when Err, <code>false</code> otherwise
      */
     boolean isErr();
 
@@ -64,28 +64,85 @@ public interface Result<E, T> {
      */
     <F> Result<F, T> mapErr(Function<E, F> mappingFn);
 
+    /**
+     * Applies the mapping function to the Ok-value, i.e. transforms T -> Result<E, U>. Use this for chaining
+     * operations that produce Results.
+     *
+     * @param mappingFn the operation that takes T and returns a Result<E, U>
+     * @param <U> the new wrapped Ok-value type
+     * @return either Err(E) or Ok(U)
+     */
     <U> Result<E, U> andThen(Function<T, Result<E, U>> mappingFn);
 
+    /**
+     * Provides Ok-value T or throws an exception if this is an Err
+     * @return T or throws {@link IllegalUnwrap} exception containing the Err type
+     */
     T unwrap();
 
+
+    /**
+     * Provides Err-value E or throws an exception if this in an Ok
+     * @return E or throws {@link IllegalUnwrap} exception containing the toString representation of Ok value
+     */
     E unwrapErr();
 
+    /**
+     * Provides the Ok-value if this Result is an Ok, otherwise provides given <code>or</code> if this is an Err
+     * @param or the T value to return if this is an Err
+     * @return either Ok-value or provided <code>or</code>
+     */
     T unwrapOr(T or);
 
+    /**
+     * provides the Ok-value if this Result is an Ok or applies the provided function on the Err-value to return a new
+     * value of type T
+     * @param elseFn function to produce a new T value from Err-value
+     * @return value of type T
+     */
     T unwrapOrElse(Function<E, T> elseFn);
 
 
+    /**
+     * If this Result is an Ok, returns the provided <code>andResult</code>, otherwise returns self
+     * @param andResult the provided Result
+     * @param <U> Ok-value type of the returned Result
+     * @return either this (Err(E)) or the provided <code>andResult</code>
+     */
     <U> Result<E, U> and(Result<E, U> andResult);
 
+    /**
+     * If this Result in an Err, returns the provided <code>orResult</code>, otherwise returns self
+     * @param orResult the provided Result
+     * @param <F> Err-value type of the returned Result
+     * @return either this (Ok(T) or the provided <code>orResult</code>
+     */
     <F> Result<F, T> or(Result<F, T> orResult);
 
+    /**
+     * If this Result is an Ok, simply returns it, otherwise if this Result is an Err, applies the provided mapping
+     * function to the Err-value to return a new Result.
+     * @param mappingFn the provided mapping function
+     * @param <F> the Err-value type of the returned result
+     * @return either this (Ok(T)) or the Result from applying the provided mapping function to the Err-value
+     */
     <F> Result<F, T> orElse(Function<E, Result<F, T>> mappingFn);
 
+    /**
+     * Applies given consumer on this Result and then returns this Result. Consumer should not modify this Result.
+     * Mainly intended for debugging/inspection of this Result
+     * @param consumer to be applied on this Result
+     * @return this
+     */
     default Result<E, T> inspect(Consumer<Result<E, T>> consumer) {
         consumer.accept(this);
         return this;
     }
 
+    /**
+     * Exception that is thrown {@link Result#unwrap()} is invoked on an Err, or {@link Result#unwrapErr()} is
+     * invoked on an Ok
+     */
     class IllegalUnwrap extends RuntimeException {
         IllegalUnwrap(String msg) {
             super(msg);
